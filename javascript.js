5b939c1f5f34f1a89f76ed9b4acf4ba94ed3cb3b
Adeebbm <adeebboumoghlbey@gmail.com>
2025-01-30 13:13:56 +0200

    change js structure for more understandable code
    Old code used regular functions whoever know this code
    will use object calculator which would contain the
    basic variables and needed functions for the calculator
    making the code more understandable

--- javascript.js
+++ javascript.js
@@ -1,18 +1,61 @@
-let numbers = document.getElementsByClassName("number");
-let operators = document.getElementsByClassName("operator");
-let display = document.getElementsByClassName("display")[0];
-let firstNumber = "", secondNumber = "",operator = "";
-
-Array.from(numbers).forEach(number => {
-    number.addEventListener('click', function() {
-        if(operator.textContent === ""){
-            firstNumber += number.textContent;
-            display.textContent = firstNumber;
+let calculator = {
+    firstNumber : "",
+    secondNumber : "",
+    operator : "",
+    display : document.getElementsByClassName("display")[0],
+    clear : function(){
+        this.firstNumber = "";
+        this.secondNumber = "";
+        this.operator = "";
+        this.display.textContent = "";
+    },
+    add : function(){
+        if(this.firstNumber === "")return;
+        if(this.operator === ""){
+            this.operator = "+";
+            this.display.textContent += this.operator;
         }else{
-            secondNumber += number.textContent;
-            display.textContent = secondNumber;
+            if(this.secondNumber === "")return;
+            this.operate();
+            this.operator = "+";
+            this.display.textContent += this.operator;
         }
-    });
-});
-
+    },
+    subtract : function(){
+        if(this.firstNumber === "")return;
+        if(this.operator === ""){
+            this.operator = "-";
+            this.display.textContent += this.operator;
+        }else{
+            if(this.secondNumber === "")return;
+            this.operate();
+            this.operator = "-";
+            this.display.textContent += this.operator;
+        }
+    },
+    multiply : function(){
+        if(this.firstNumber === "")return;
+        if(this.operator === ""){
+            this.operator = "×";
+            this.display.textContent += this.operator;
+        }else{
+            if(this.secondNumber === "")return;
+            this.operate();
+            this.operator = "×";
+            this.display.textContent += this.operator;
+        }
+    },
+    divide : function(){
+        if(this.firstNumber === "")return;
+        if(this.operator === ""){
+            this.operator = "÷";
+            this.display.textContent += this.operator;
+        }else{
+            if(this.secondNumber === "")return;
+            this.operate();
+            this.operator = "÷";
+            this.display.textContent += this.operator;
+        }
+    },
+}
 
